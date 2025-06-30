99eba4d08bff46886a557c6179a310ae489bac5c
Nayaker <nayak.primary@gmail.com>
2025-06-30 17:56:06 +0530

    bug fix complete - temp

--- server/app.js
+++ server/app.js
@@ -21,6 +21,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
+app.use(express.json({ limit: '100mb' })); // For JSON bodies
+app.use(express.urlencoded({ limit: '100mb', extended: true })); // For URL-encoded
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
